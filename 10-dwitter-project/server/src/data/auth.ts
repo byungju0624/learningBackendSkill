1d11a8f00d8f86294fb5e913f1a650a1f1fa5a0f
byungju0624 <byungju0624@gmail.com>
2021-12-10 09:07:55 +0900

    .

--- 10-dwitter-project/server/src/data/auth.ts
+++ 10-dwitter-project/server/src/data/auth.ts
@@ -1,32 +1,21 @@
 // abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
 type User = {
   id: string;
-  username: string;
+
   password: string;
-  name: string;
+
   email: string;
-  url?: string;
 };
 let users: User[] = [
   {
     id: "1",
-    username: "bob",
-    password: "$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm",
-    name: "Bob",
     email: "bob@gmail.com",
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
-  },
-  {
-    id: "2",
-    username: "ellie",
     password: "$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm",
-    name: "Ellie",
-    email: "ellie@gmail.com",
   },
 ];
 
-export async function findByUsername(username: string) {
-  return users.find((user) => user.username === username);
+export async function findByUsername(email: string) {
+  return users.find((user) => user.email === email);
 }
 
 export async function findById(id: string) {
